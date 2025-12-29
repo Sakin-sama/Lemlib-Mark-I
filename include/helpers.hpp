64249bfcb346ce087ee4d773484c838e43ba6be3
Sakin-sama <sakinsocials@proton.me>
2025-12-28 22:44:47 -0500

    idk anymore

--- include/helpers.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "globals.hpp"
-
-//Functions
-double motorVelocity(double givenVelocity);
-void intakeControls();
-void colorSorting(std::string goodColor);
-void mandibleControls();
